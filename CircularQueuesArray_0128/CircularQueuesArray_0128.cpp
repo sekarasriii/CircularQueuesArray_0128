56e61034649525fdb8128fd3e43539ff4d2a3d48
sekarasriii <sekareeesh@gmail.com>
2024-05-31 11:01:27 +0700

    membuat fungsi main

--- CircularQueuesArray_0128/CircularQueuesArray_0128.cpp
+++ CircularQueuesArray_0128/CircularQueuesArray_0128.cpp
@@ -71,16 +71,23 @@ public:
 			}
 			cout << endl;
 		}
-		else{
+		else {
 			while (FRONT_position <= max - 1) {
 				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
 
-		FRONT_position = 0;
+			FRONT_position = 0;
 
-		while (FRONT_position <= REAR_position) {
-
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 	}
 };
+
+int main() {
+
+}
